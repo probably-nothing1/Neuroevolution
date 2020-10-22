0cabe99bdb77de2e4d514a7920920c5311dc1ab0
gasiortomasz <gasior.tt@gmail.com>
2020-10-22 17:40:37 +0200

    Add efficient parallel (single node) implementation

---
+++ src/main_parallel_one_node.py
@@ -0,0 +1,153 @@
+import copy
+import sys
+import time
+from collections import namedtuple
+from itertools import count
+from random import randint
+
+import gin
+import numpy as np
+import torch
+import torch.multiprocessing as mp
+import wandb
+
+from model import create_model
+from utils.env_utils import create_environment
+from utils.multiprocessing_utils import (
+    EMPTY_MESSAGE,
+    KILL_MESSAGE,
+    ResultItem,
+    WorkItem,
+    collect_results,
+    kill_processes,
+    spawn_processes,
+)
+from utils.pytorch_utils import create_noise_tensors
+from utils.utils import randint_generator, set_seed, unzip
+
+
+def evaluate(model, env):
+    total_reward = 0
+    o = env.reset()
+    done = False
+    while not done:
+        o_tensor = torch.FloatTensor(o).unsqueeze(0)
+        a = model(o_tensor)
+        o, reward, done, _ = env.step(a)
+        total_reward += reward
+
+    return total_reward
+
+
+@gin.configurable
+def mutate(model, noise_std, seed):
+    new_model = copy.deepcopy(model)
+    noise = create_noise_tensors(new_model, seed)
+    for p, noise_p in zip(new_model.parameters(), noise):
+        p.data += noise_p * noise_std
+    return new_model
+
+
+def mutate_and_evaluate_task(elite, work_queue, results_queue):
+    env = create_environment()
+    while True:
+        work_item = work_queue.get()
+        model_id, seed, message = work_item
+        if message == KILL_MESSAGE:
+            return
+
+        model = elite[model_id]
+        new_model = mutate(model, seed=seed)
+
+        fitness = evaluate(new_model, env)
+        result_item = ResultItem(model_id=model_id, seed=seed, fitness=fitness)
+        results_queue.put(result_item)
+
+
+def spawn_mutation_work(queue, elite_size, size):
+    for seed in randint_generator(size):
+        model_id = randint(0, elite_size - 1)
+        queue.put(WorkItem(model_id=model_id, seed=seed, message=EMPTY_MESSAGE))
+
+
+def sort_population(evaluated_population):
+    evaluated_population.sort(key=lambda result: result.fitness, reverse=True)
+
+
+def mutate_models(elite_models, elite_ids, seeds):
+    copied_models = [mutate(elite_models[elite_id], seed=seed) for elite_id, seed in zip(elite_ids, seeds)]
+    for elite, mutated_elite in zip(elite_models, copied_models):
+        elite.load_state_dict(mutated_elite.state_dict())
+        del mutated_elite
+
+
+def get_top_performers(evaluated_population, elite, elite_size):
+    sort_population(evaluated_population)
+    top_ids, top_seeds, top_scores = unzip(evaluated_population[:elite_size])
+    mutate_models(elite, top_ids, top_seeds)
+    return np.array(top_scores)
+
+
+def log_generation_stats(generation, scores, time):
+    print(f"{generation}. {scores.mean():.2f} +-{scores.std():.2f}. t={time:.2f} seconds.")
+    # wandb.log(
+    #     {
+    #         "Generation": generation,
+    #         "Generation Time": elapsed_time,
+    #         "Max Reward": scores.max(),
+    #         "Mean Reward": scores.mean(),
+    #         "Std Reward": scores.std(),
+    #     }
+    # )
+
+
+def create_population(env, population_size):
+    return [create_model(env) for _ in range(population_size)]
+
+
+def get_top_performers_from_random_population(env, population, elite_size):
+    evaluated_population = [(model, evaluate(model, env)) for model in population]
+    evaluated_population.sort(key=lambda result: result[1], reverse=True)
+    elite, top_scores = unzip(evaluated_population[:elite_size])
+    for e in elite:
+        e.share_memory()
+    return elite, np.array(top_scores)
+
+
+@gin.configurable
+def train(solved_score, population_size, elite_size, num_proc):
+    manager = mp.Manager()
+    work_queue = manager.Queue()
+    results_queue = manager.Queue()
+
+    # Random Search 1st generation
+    start_time = time.time()
+    env = create_environment()
+    population = create_population(env, population_size)
+    elite, top_scores = get_top_performers_from_random_population(env, population, elite_size)
+    elapsed_time = time.time() - start_time
+    log_generation_stats(1, top_scores, elapsed_time)
+
+    # 2nd -> inf generation: Mutate Top Performers (classic GA)
+    ma_reward = 0
+    spawn_processes(num_proc, work_fn=mutate_and_evaluate_task, args=(elite, work_queue, results_queue))
+    for generation in count(start=2, step=1):
+        start_time = time.time()
+        spawn_mutation_work(work_queue, elite_size, population_size)
+
+        evaluated_population = collect_results(results_queue, size=population_size)
+        top_scores = get_top_performers(evaluated_population, elite, elite_size)
+        elapsed_time = time.time() - start_time
+        log_generation_stats(generation, top_scores, elapsed_time)
+
+        ma_reward = 0.9 * ma_reward + 0.1 * top_scores.mean()
+        if ma_reward >= solved_score:
+            print(f"Solved in {generation} generations")
+            kill_processes(work_queue, num_proc)
+            break
+
+
+if __name__ == "__main__":
+    experiment_file = sys.argv[1]
+    gin.parse_config_file(experiment_file)
+    train()
